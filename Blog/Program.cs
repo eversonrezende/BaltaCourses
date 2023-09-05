506f1b34b47a0ee2f1094a82e0a15149287fbba4
erezendeosg <erezende@osg.com.br>
2023-09-05 13:33:14 -0300

    Imersão

---
+++ Blog/Program.cs
@@ -0,0 +1,84 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{  
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var connection = new SqlConnection(CONNECTION_STRING);
+      connection.Open();
+      ReadUsers(connection);
+      //ReadUser();
+      //CreateUser();
+      //UpdateUser();
+      //DeleteUser();
+      connection.Close();
+    }
+
+    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=blog;User ID=sa;Password=1q2w3e4r@#$;Integrated Security=false;TrustServerCertificate=true";
+
+    public static void ReadUsers(SqlConnection connection)
+    {
+      var repository = new UserRepository(connection);
+      var users = repository.Get();
+
+      foreach (var user in users)
+          Console.WriteLine(user.Name);
+    }
+
+    public static void ReadUser()
+    {
+      using var connection = new SqlConnection(CONNECTION_STRING);
+      var user = connection.Get<User>(1);
+      Console.WriteLine(user.Name);
+    }
+
+    public static void CreateUser()
+    {
+      var user = new User
+      {
+        Bio = "Equipe balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe Balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe-balta"
+      };
+
+      using var connection = new SqlConnection(CONNECTION_STRING);
+      connection.Insert<User>(user);
+      Console.WriteLine("Cadastro realizado com sucesso");
+    }
+
+    public static void UpdateUser()
+    {
+      var user = new User
+      {
+        Id = 2,
+        Bio = "Equipe | balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe de suporte Balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe-balta"
+      };
+
+      using var connection = new SqlConnection(CONNECTION_STRING);
+      connection.Update<User>(user);
+      Console.WriteLine("Atualização realizada com sucesso");
+    }
+
+    public static void DeleteUser()
+    {
+      using var connection = new SqlConnection(CONNECTION_STRING);
+      var user = connection.Get<User>(2);
+      connection.Delete<User>(user);
+      Console.WriteLine("Exclusão realizada com sucesso");
+    }
+
+  }
+}
--- script-data-access.sql
+++ script-data-access.sql
@@ -37,3 +37,24 @@ AS
 
 -- Executar a procedure passando o parâmetro
 EXEC spGetCoursesByCategory '09ce0b7b-cfca-497b-92c0-3290ad9d5142';
+
+SELECT * FROM [vwCourses];
+
+SELECT * FROM [CareerItem] INNER JOIN [Course]
+ON [CareerItem].[CourseId] = [Course].[Id];
+
+SELECT 
+    [Career].[Id],
+    [Career].[Title],
+    [CareerItem].[CareerId],
+    [CareerItem].[Title]
+FROM 
+    [Career] 
+INNER JOIN 
+    [CareerItem] ON [CareerItem].[CareerId] = [Career].[Id]
+ORDER BY
+    [Career].[Title];
+
+SELECT * FROM Career WHERE Id IN ('4327ac7e-963b-4893-9f31-9a3b28a4e72b','e6730d1c-6870-4df3-ae68-438624e04c72');
+
+SELECT * FROM Career;
